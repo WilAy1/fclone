ea364e8139a60b21f6e3843a5b0593be08c21b70
wilay <ayomikunakintade@gmail.com>
2022-07-23 21:44:38 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-import logo from './images/foodlrlogo.png'
+import logo from './images/foodlrlogo.png';
+import gif from './images/download.gif'
 function App() {
   return (
     <div className="App">
       <header className='pageHeaded'>
-        <nav className='fNavBar'>
-          <div className='fLogo'>
-            <img src={logo} className="fLogoImg" alt='Logo'/>
-          </div>
-        </nav>
+          <img src={gif} className="fLogoImg" alt='Logo'/>
+          {/*<div className='comingSoon'>Coming Soon</div>*/}
       </header>
-
     </div>
   );
 }
